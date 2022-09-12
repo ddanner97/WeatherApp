6e920340f87040927ab5708f57d7088c2f2a9a89
Damian <ddanner97@gmail.com>
2022-09-12 11:46:35 -0500

    add conditional rendering to MainDisplay page

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import WeatherIcon from './icons/WeatherIcon'
 import WeatherInfo from './WeatherInfo'
+import Loader from './icons/Loader'
 
 export default class MainDisplay extends Component {
 
@@ -89,11 +90,18 @@ export default class MainDisplay extends Component {
     render() {
         return (
             <div className="main-display-container">
-                <h2 className="main-display-name">
-                    {this.state.name}
-                </h2>
-                <WeatherIcon icon={this.state.icon} />
-                <WeatherInfo temp={this.state.temp} feelsLike={this.state.feelLike} humidity={this.state.humidity} />
+                {/* Ternary operator: If loading == false render loading, If error == render error, else render page */}
+                { !this.state.isLoaded ? <Loader/>
+                    : this.state.error ? <div>Error</div>
+                        :
+                        <div className="main-display-container">
+                            <h2 className="main-display-name">
+                                {this.state.name}
+                            </h2>
+                            <WeatherIcon icon={this.state.icon} />
+                            <WeatherInfo temp={this.state.temp} feelsLike={this.state.feelLike} humidity={this.state.humidity} />
+                        </div>
+                }
             </div>
         )
     }
